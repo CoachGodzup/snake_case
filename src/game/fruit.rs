95ac9cd342349e1bfd28334102d2ab6eb51b2470
Coachgodzup <godzup@gmail.com>
2024-04-29 21:47:41 +0200

    super-aggro ai

--- src/game/fruit.rs
+++ src/game/fruit.rs
@@ -1,6 +1,7 @@
-use bracket_lib::{color::{ALICEBLUE, BLANCHED_ALMOND}, terminal::{to_cp437, BTerm, Point}};
+use bracket_lib::{color::BLANCHED_ALMOND, terminal::{to_cp437, BTerm, Point}};
 
 use super::snake_game_state::BACKGROUND_COLOR;
+#[derive(Clone, Copy, PartialEq)]
 
 pub struct Fruit {
     pub position: Point,
